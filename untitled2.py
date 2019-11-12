ca9fe448aa780fcdf7581170521c2778e610b72f
sicTa <nikola4tasic@hotmail.com>
2019-11-12 20:40:15 +0100

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -6,8 +6,17 @@ Created on Tue Nov  5 15:44:28 2019
 """
 
 import AudioAnalyzer
+import numpy as np
 
 aa = AudioAnalyzer.AudioAnalyzer()
 mfcc = aa.MFCC('OSR_us_000_0061_8k.wav')
+aa.plot(mfcc)
 
 
+#using mfcc's we can make a simple list 
+
+x = aa.feature_vector2('OSR_us_000_0061_8k.wav')
+
+
+#print(mfcc[100])
+print(x[100])
